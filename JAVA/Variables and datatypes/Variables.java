ce0b3d87a6d7084b3a948f310bbb2a6d3b684070
Mueza Ejaz <159851369+Mueza-Ejaz@users.noreply.github.com>
2025-07-02 16:17:06 +0500

    variables and datatypes

---
+++ JAVA/Variables and datatypes/Variables.java
@@ -0,0 +1,91 @@
+
+import java.util.*;
+
+public class Variables {
+    public static void main(String[] args){
+        
+    // output Printing Ststement
+
+        System.out.println("*"); 
+        System.out.print("**\n");
+        System.out.print("***\n");
+        System.out.print("****\n");
+
+        // cli output:
+        // *
+        // **
+        // ***
+        // ****
+
+//----------------------------------- Variales and Data Types -----------------------------------------//
+
+
+        String name = "Mueza";
+        System.out.println("My name is: " + name);
+        
+        int a = 10;
+        int b = 5;
+
+        int calculate = (a * b) / (a - b); //10
+
+        System.out.println("Total:" + calculate);
+
+
+
+        // for next string
+        // String name = sc.next();          // input = mueza ejaz and output = mueza
+        // String name = sc.nextLine();     // input = mueza ejaz and output = mueza ejaz
+
+        // //nextInt()
+        //nextFloat()
+
+
+
+// practice question:
+            //Input printing Statement
+
+            Scanner sc = new Scanner(System.in);
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int sum = a + b;
+
+        System.out.println("Sum: " + sum);
+
+
+// cli Output:
+// 2
+//  4
+// Sum: 6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
