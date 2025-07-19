6d8e9cd1063d206ac7e44e3593a3d9ce96a1a828
Mueza Ejaz <159851369+Mueza-Ejaz@users.noreply.github.com>
2025-07-19 12:28:45 +0500

    Add files via upload

---
+++ JAVA/String and StringBuilder/Strings.java
@@ -0,0 +1,139 @@
+import java.util.*;
+
+
+public class Strings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Declaration
+            // String name = "Mueza";
+
+        // Taking input
+        // String name = sc.next();
+        // mueza ejaz => input
+         // mueza = > output
+
+        // String name = sc.nextLine(); // output: Mueza ejaz
+        // System.out.println(name);
+
+        //-----------------------------------------------------------//
+
+//        // print lenght of a string
+//        String sub = "Physics";
+//        System.out.print(sub.length());
+
+        //output: 7
+
+        //-----------------------------------------------------------//
+
+        //Access characters
+
+//        String sub = "Physics";
+//        System.out.println(sub.length());
+//
+//        for(int i =0; i < sub.length(); i++){
+//            System.out.println(sub.charAt(i));
+//        }
+
+        //output:
+//        7
+//        P
+//        h
+//        y
+//        s
+//        i
+//        c
+//        s
+
+
+        //-----------------------------------------------------------//
+        // compare function check three cases:
+            // 1 s1 > s2 : +ve value
+            // 2 s1 < s2 : 0
+            //3 s1 < s2  : -ve value
+
+//        String name1 = "mueza";
+//        String name2 = "mueza2";
+//
+//        if(name1.compareTo(name2) == 0){
+//            System.out.println("Strings are equal");
+//
+//        }else{
+//            System.out.println("Strings are not equal");
+//        }
+
+        //-----------------------------------------------------------//
+
+//        //Substring:
+//        String name = "mueza ejaz";
+//        System.out.println(name.substring(0,6)); // mueza
+
+        //-----------------------------------------------------------//
+
+        //ParseInt Method of Integer class
+
+//        String str = "2";
+//        int number = Integer.parseInt(str);
+//        System.out.println(number);
+
+
+        //-----------------------------------------------------------//
+
+        // ToString Method of String class
+
+//        int number = 123;
+//        String str = Integer.toString(number);
+//        System.out.println(str.length());  // 3
+
+
+
+
+        sc.close();
+    }
+}
+
+
+// output:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
